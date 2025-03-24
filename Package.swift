3eedeb8277fc232f2cd4c80b28ec422e5751b2ae
ayushshrivastv <ayush.srivastav@icloud.com>
2025-03-24 22:14:18 +0530

    change

--- Package.swift
+++ Package.swift
@@ -1,6 +1,7 @@
 // swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 // swift-tools-version:5.7
+//MIT License Copyright (c) 2024 Ayush Srivastava
 import PackageDescription
 
 let package = Package(
